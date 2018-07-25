04470ba799963f127255e2cad9bfb5d2045d8fa7
Sanjaya <sanjaya@graype.in>
2018-07-25 15:18:42 +0530

    chore(*): update copyright notice
    closes #4

--- src/probability-aggregate.js
+++ src/probability-aggregate.js
@@ -1,23 +1,23 @@
 //     wink-statistics
 //     Fast and Numerically Stable Statistical Analysis Utilities.
 //
-//     Copyright (C) 2017  GRAYPE Systems Private Limited
+//     Copyright (C) 2017-18  GRAYPE Systems Private Limited
 //
-//     This file is part of “wink-utils”.
+//     This file is part of “wink-statistics”.
 //
-//     “wink-utils” is free software: you can redistribute it
+//     “wink-statistics” is free software: you can redistribute it
 //     and/or modify it under the terms of the GNU Affero
 //     General Public License as published by the Free
 //     Software Foundation, version 3 of the License.
 //
-//     “wink-utils” is distributed in the hope that it will
+//     “wink-statistics” is distributed in the hope that it will
 //     be useful, but WITHOUT ANY WARRANTY; without even
 //     the implied warranty of MERCHANTABILITY or FITNESS
 //     FOR A PARTICULAR PURPOSE.  See the GNU Affero General
 //     Public License for more details.
 //
 //     You should have received a copy of the GNU Affero
-//     General Public License along with “wink-utils”.
+//     General Public License along with “wink-statistics”.
 //     If not, see <http://www.gnu.org/licenses/>.
 
 // ## probability
